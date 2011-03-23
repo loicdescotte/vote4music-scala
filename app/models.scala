5a14a017cd32f566c6f88eb2f53c40b8e33306ec
unknown <Loic@.(none)>
2011-03-23 22:23:07 +0100

    scala version for vote4music

---
+++ app/models.scala
@@ -0,0 +1,50 @@
+package controllers
+
+import java.util._
+import javax.persistence._
+import play.db.jpa.Model
+import play.data.validation.Required
+import javax.persistence.{EnumType, TemporalType, CascadeType}
+
+class Album(
+             @Required var name: String,
+             @ManyToOne(cascade = { CascadeType.PERSIST}) var artist: Artist,
+             @Temporal(TemporalType.DATE) @Required var releaseDate: Date,
+             @Enumerated(EnumType.STRING)
+             var genre : Genre,
+             var nbVotes: Long = OL,
+             var hasCove: Boolean = false)
+  extends Model {
+
+  /*
+  def this(name:String)={
+    this(name,null,null,null,0L,false);
+  } */
+
+  /*
+   * Remove duplicate artist
+     * @return found duplicate artist if exists
+     */
+  def replaceDuplicateArtist = {
+    def existingArtists: Artist = Artist.findByName(artist.name)
+    if (existingArtists.size() > 0) {
+      //Artist name is unique
+      artist = existingArtists.get(0)
+    }
+  }
+
+}
+
+
+class Artist(
+@Required @Column(unique = true) var name:String) extends Model {
+      def findByName(name:String) = {
+        find("byName", name).fetch();
+    }
+}
+
+sealed trait Genre
+final case object ROCK extends Genre
+final case object METAL extends Genre
+final case object POP extends Genre
+
